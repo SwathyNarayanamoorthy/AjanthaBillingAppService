6b91f520244c79e83ebde84d8eed80dd92611a23
ADVISORY\narayansw <narayansw@advisory.com>
2019-04-20 17:22:21 +0530

    NAvigate to yourproducts

--- FEE/app/actions/index.js
+++ FEE/app/actions/index.js
@@ -2,7 +2,7 @@
 import * as actionEvents from './events';
 import * as dataRequests from './dataRequests';
 
-import {  push } from 'react-router-redux';
+import history from '../history';
 export function successLoginUser (data) {
   return {
     type: actionEvents.USER_AUTHENTICATED_SUCCESSFULY,
@@ -22,7 +22,7 @@ export function authenticateUser (username, password) {
   return function (dispatch) {
     return dataRequests.authenticateUser(username, password)
       .then(function (response) {
-        dispatch(push(`/users/${response.userId}`));
+        dispatch(history.push(`/users/${response.userId}`));
         dispatch(successLoginUser(response));
       })
       .catch(function (error) {
@@ -30,7 +30,7 @@ export function authenticateUser (username, password) {
           userId: '1234',
           username: 'Swat'
         }
-        dispatch(push(`/users/123`));
+        dispatch(history.push(`/users/123`));
         dispatch(successLoginUser(response));
         //dispatch(dispatchErrorHandler(actionEvents.FAILED_USER_AUTHENTICATION, error));
       });
--- FEE/app/index.js
+++ FEE/app/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
 import ReactDOM from 'react-dom'
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Router } from 'react-router-dom'
 import Dashboard from './views/dashboard'
 import Login from './views/login'
+import { hashHistory  } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory()
 const routing = (
-  <Router>
+  <Router history={history}>
     <div>
-      <Route path="/" component={Login} />
+      <Route exact path="/" component={Login} />
       <Route path="/login" component={Login} />
-      <Route path="/users/123/" component={Dashboard} />
+      <Route path="/users/*/" component={Dashboard} />
     </div>
   </Router>
 )
